eed7c0d9f9059255dee5079dedef5e6f35492d48
CAT uwu <88683832+CAT9030@users.noreply.github.com>
2022-04-03 23:37:07 -0500

    c5

--- model/Songs.js
+++ model/Songs.js
@@ -4,8 +4,8 @@ class Songs {
             {
                 id: 0,
                 name:"Let's Go",
-                artist: "Sumamá",
-                album: "Nosé Vol.1",
+                artist: "Headband",
+                album: "On That",
                 file: loadSound('./songs/Lets Go.mp3'),
                 img: loadImage('./images/image_3headband.png')
             },
@@ -13,8 +13,8 @@ class Songs {
             {
                 id: 1,
                 name:"Know My Name",
-                artist: "Sumamá2",
-                album: "Nosé Vol.2",
+                artist: "The Derevolutions",
+                album: "Vol.2",
                 file: loadSound('./songs/Song2.mp3'),
                 img: loadImage('./images/image_2now_you_know_my_name.png')
             },
@@ -22,8 +22,8 @@ class Songs {
             {
                 id: 2,
                 name:"Fight The War",
-                artist: "Sumamá",
-                album: "Nosé Vol.1",
+                artist: "Kipo Soundtrack",
+                album: "Mixtape 1",
                 file: loadSound('./songs/Fight The War.mp3'),
                 img: loadImage('./images/image_4.png')
             },
@@ -31,8 +31,8 @@ class Songs {
             {
                 id: 3,
                 name:"Purple Jaguar Eye",
-                artist: "Sumamá",
-                album: "Nosé Vol.1",
+                artist: "Sterling K. Brown",
+                album: "Mixtape 2",
                 file: loadSound('./songs/Purple Jaguar Eye.mp3'),
                 img: loadImage('./images/image_5.png')
             },
@@ -40,8 +40,8 @@ class Songs {
             {
                 id: 4,
                 name:"Heroes On Fire",
-                artist: "Sumamá",
-                album: "Nosé Vol.1",
+                artist: "Karen Fukahara",
+                album: "Mixtape 3",
                 file: loadSound('./songs/Heroes On Fire.mp3'),
                 img: loadImage('./images/image_6.png')
             },
@@ -49,8 +49,8 @@ class Songs {
             {
                 id: 5,
                 name:"Don't Stop Now",
-                artist: "Sumamá",
-                album: "Nosé Vol.1",
+                artist: "Kipo Soundtrack",
+                album: "Vol.1",
                 file: loadSound('./songs/Dont Stop Now.mp3'),
                 img: loadImage('./images/image_4.png')
             },
@@ -58,8 +58,8 @@ class Songs {
             {
                 id: 6,
                 name:"Crumbs",
-                artist: "Sumamá",
-                album: "Nosé Vol.1",
+                artist: "Jordan Dennis",
+                album: "Crumbs",
                 file: loadSound('./songs/Crumbs.mp3'),
                 img: loadImage('./images/image_1crumbs.png')
             },
